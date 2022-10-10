c3999cdb8791f570f7e895aafb8a357bcd38f020
MAGSON MARTINS MAGALHÃES <MMMAGAL@trf3.jus.br>
2022-10-10 17:18:41 -0400

    Estrutura Inicial de Pastas do Projeto

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Button } from "@react-native-material/core";
+
+const App = () => (
+    <Button title="Click Me" style={{ alignSelf: "center", marginTop: 40 }}/>
+);
+
+export default App;
